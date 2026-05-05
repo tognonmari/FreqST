d1ae5b627835cd14e264384bccfed48e98c27460
tognonmari <mariafiore.tognon@gmail.com>
2026-05-05 14:33:50 +0200

    committing to merge

--- vc_dim_extractor.h
+++ vc_dim_extractor.h
@@ -476,7 +476,7 @@ class vc_dim_extractor{
                 children_to_explore.push_back(it);
             }
             int global_generations = 0;
-            #pragma omp parallel num_threads(2)
+            #pragma omp parallel num_threads(6)
             {
                 std::vector<RangeRoaringBitmap<space>> local_candidates;
                 std::vector<RangeRoaringBitmap<space>> temporary_candidates;
@@ -609,7 +609,7 @@ class vc_dim_extractor{
             for (const auto& [key, data] : F_1){
                 tids.push_back(*(key.get_range().begin()));
             }
-            #pragma omp parallel num_threads(2)
+            #pragma omp parallel num_threads(6)
             {
                 std::map<RangeRoaringBitmap<space>, transaction_set_data> local_F_2;
                 #pragma omp for schedule(dynamic, 8) 
@@ -646,7 +646,7 @@ class vc_dim_extractor{
             }
         };
         static void generate_pairs_from_supports(Trie& T_2, std::map<RangeRoaringBitmap<space>, transaction_set_data>& F_2,const std::vector<RangeRoaringBitmap<space>>& set_of_supports, const std::vector<roaring::Roaring>& inverted_index){
-            #pragma omp parallel num_threads(2)
+            #pragma omp parallel num_threads(6)
             {
                 int tid = omp_get_thread_num();
                 int nthreads = omp_get_num_threads();
@@ -754,7 +754,7 @@ class vc_dim_extractor{
                         count = 0;
                     }
                     count++;
-                    std::cout << s.to_string() <<std::endl;
+                    //std::cout << s.to_string() <<std::endl;
 
                 }
                 //set becomes a vector for parallelization later on 
