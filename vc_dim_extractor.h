ba3710e3203281ce6f6e416502d71f8670c19d6d
tognonmari <mariafiore.tognon@gmail.com>
2026-05-06 11:30:13 +0200

    Restored old version which outperforms the new one ( apparently). I suspect this is due to the aggressive filtering.

--- vc_dim_extractor.h
+++ vc_dim_extractor.h
@@ -579,10 +579,11 @@ class vc_dim_extractor{
             roaring::Roaring trajectory_ids = candidate.get_range();
             roaring::Roaring filtered_ranges;
             //roaring::Roaring filtered_ranges;
+            std::vector<id_t> elems;
             //std::cout<< std::format("--------------checking shattering for set {}, for which i need {} sets. \n", candidate.to_string(), POWERS_OF_TWO[candidate.count()+1]-1);
             for (auto it = trajectory_ids.begin(); it != trajectory_ids.end(); ++it) {
-                
-                filtered_ranges |= inverted_index[*it];
+                elems.push_back(*it);
+                //filtered_ranges |= inverted_index[*it];
             }
             //std::cout << "FILTERED RANGES: \n";
             //for (const auto item: filtered_ranges){
@@ -590,15 +591,16 @@ class vc_dim_extractor{
             //    std::cout << ranges[item].to_string()<< "\n";
 
             //}
+            /*
             std::set<RangeRoaringBitmap<space>> found_ranges;
             
             for (const auto item : filtered_ranges ){
 
                 RangeRoaringBitmap<space> intersection(candidate.get_range() & ranges[item].get_range());
-                if(found_ranges.find(intersection)== found_ranges.end()){
+                //if(found_ranges.find(intersection)== found_ranges.end()){
                     //std::cout<< std::format("I can realize subset {} of set {} with range {}\n",intersection.to_string(), candidate.to_string(), ranges[item].to_string());
-                    found_ranges.insert(intersection);
-                }
+                found_ranges.insert(intersection);
+                //}
                 if(found_ranges.size() == (POWERS_OF_TWO[candidate.count()+1]-1)){
                     //std::cout<< std::format("I have found {} ranges, hence i can shatter set {} of size {}.\n", found_ranges.size(), candidate.to_string(), candidate.count() );
                     return true;
@@ -607,7 +609,8 @@ class vc_dim_extractor{
             }
             
             return false;
-            /*
+            */
+            int n = elems.size();
             // Iterate over all non-empty subsets
             for (uint64_t mask = 1; mask < (1ULL << n); ++mask) {
                 
@@ -642,7 +645,7 @@ class vc_dim_extractor{
             }
             //std::cout<<std::format("Set {} is shattered.\n", candidate.to_string());
             return true;
-            */
+            
         };
         static void generate_pairs_by_enumeration(Trie& T_2, std::map<RangeRoaringBitmap<space>, transaction_set_data>& F_2,std::map<RangeRoaringBitmap<space>, transaction_set_data>& F_1,std::vector<roaring::Roaring> inverted_index, int threads){
 
