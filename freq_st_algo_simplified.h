592aec0d7337eb793215709b2bb5caeb54ed9993
tognonmari <mariafiore.tognon@gmail.com>
2026-05-06 11:46:33 +0200

    to merge

--- freq_st_algo_simplified.h
+++ freq_st_algo_simplified.h
@@ -679,7 +679,7 @@ class frequent_subtrajectory_algo_simplified{
                     }
 
                     int count = fsg.query_one_pathlet_over_the_sample_with_labels(sample, pn.getPathlet(), this->integer_frequency_threshold); //SF IS HERE
-                    int count = 0;
+                    
                     if (pruning_enabled){
                         count = fsg.query_one_pathlet_over_the_sample_with_labels(sample, pn.getPathlet(), integer_frequency_threshold);
                     }
